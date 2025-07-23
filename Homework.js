8746433fc2d6ac6699a627ef4fa6931d750d2f6e
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-07-23 12:42:20 +0300

    Add files via upload

---
+++ Homework.js
@@ -0,0 +1,5 @@
+let Myname = "Ahmad";
+let FavoriteSubject = "Maths";
+let grade = "6";
+let nationality = "Pakistan";
+console.log(`My name is  ${Myname}. I am from ${nationality}. My favorite subject is ${FavoriteSubject}. I study in grade ${grade}. `) 
