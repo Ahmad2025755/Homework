d6e380f43b9c6a60108ea9b8a4aca84d7498f2df
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-07-29 12:55:11 +0300

    Add files via upload

---
+++ HobbiesJavascript.js.js
@@ -0,0 +1,14 @@
+function Sharing(){
+
+ var Personhobby = document.getElementById("name").value;
+ alert("Bro, nobody in these days plays  " + Personhobby);
+}
+
+function goDark(){
+
+    document.body.className = "dark";
+}
+
+function End(){
+    document.getElementById("Hobbies").innerHTML = "After finding out your hobby, I was sad! :(";
+}
