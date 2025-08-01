79e1e6fa3a83758c13d18e10ec6bf7667348e15c
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-08-01 16:06:08 +0300

    Add files via upload

---
+++ HomeworkPortfolio.js
@@ -0,0 +1,9 @@
+function About() {
+
+document.getElementById("Aboutme-info").innerHTML =
+
+"Hello! My name is Ahmad Abdullah and I study in grade 5. This year, I will be transfering to grade 6. I live in Pakistan. I am 11 years old, and my best friends names are Sabaoon, Abdullah, and Ibaad!"
+"asda"
+
+
+}
